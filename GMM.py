edf15ea3effb3050efe6efc9423258c0d7eba38a
4jgm2000 <4jgm2000@gmail.com>
2021-07-07 12:47:32 -0400

    gmm list shortened to -2019

--- GMM.py
+++ GMM.py
@@ -16,9 +16,21 @@ for (key,value) in stocks.items():
         cur = value['Date']
         indList.append(cur.index('2019-01-02', -700, -1))
         # cur = pd.DataFrame.from_dict(value['Date'])
-
+        del value['Date']
     except Exception:
         indList.append(0)
+        del value['Date']
 print(indList[0:100])
+
+
+
+inputArray = pd.DataFrame.from_dict(stocks, orient='index')
+i=0
+for (key,value) in stocks.items():
+    cur = inputArray.loc[key].at['Open']
+    inputArray.at[key,'Open'] = cur[0:indList[i]]
+    i+=1 
+print(indList[0], len(inputArray.loc['A'].at['Open']))
+print(inputArray.loc['A'].at['Open'])
 # gm = GaussianMixture(n_components=3, random_state=0).fit_predict(inputArray)
 
