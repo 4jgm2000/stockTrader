6255caae1ce885e1cc600398810129c540fce271
4jgm2000 <4jgm2000@gmail.com>
2021-07-07 15:52:57 -0400

    asdsafd

--- GMM.py
+++ GMM.py
@@ -9,7 +9,7 @@ with open('reduced_dimension_stock_data.json') as json_file:
     stocks = json.load(json_file)
 
 indList = []
-print(len(stocks))
+#print(len(stocks))
 for (key,value) in stocks.items():
     try:
         # print(value['Date'])
@@ -20,17 +20,55 @@ for (key,value) in stocks.items():
     except Exception:
         indList.append(0)
         del value['Date']
-print(indList[0:100])
+#print(indList[0:100])
 
 
 
 inputArray = pd.DataFrame.from_dict(stocks, orient='index')
+#find longest sub-array
+longest = 0
+for (key, value) in stocks.items():
+    if (len(inputArray.loc[key].at['Open']) > longest):
+        longest = len(inputArray.loc[key].at['Open'])
+#print("longest:")
+#print(longest)
+#in this dataset, longest is 14665
+
 i=0
 for (key,value) in stocks.items():
     cur = inputArray.loc[key].at['Open']
     inputArray.at[key,'Open'] = cur[0:indList[i]]
-    i+=1 
-print(indList[0], len(inputArray.loc['A'].at['Open']))
-print(inputArray.loc['A'].at['Open'])
-# gm = GaussianMixture(n_components=3, random_state=0).fit_predict(inputArray)
-
+    #duplicate first element to make array lengths match
+    # print(inputArray.at[key,'Open'][0])
+    try:
+        firstEle = inputArray.at[key,'Open'][0]
+        # print(firstEle, key)
+        
+        # print(firstEle != '0.0', firstEle)
+        if (firstEle != '0.0'):
+            cre = numpy.array([firstEle for j in range(longest - len(inputArray.at[key, 'Open']))])
+            # print(cre)
+            inputArray.at[key,'Open'] = numpy.append(cre,inputArray.at[key,'Open'])
+            i+=1
+            # print(len(inputArray.at[key,'Open']) == longest)
+        else:
+            # i+=1
+            # print(i)
+            # inputArray.drop(i,axis=0)
+            inputArray = inputArray.drop(key)
+            i+=1
+    except Exception:
+        # i+=1
+        # print(i)
+        # print('here')
+        inputArray = inputArray.drop(key)
+        i+=1
+print(len(inputArray))
+arr = pd.DataFrame.to_numpy(inputArray)
+print(numpy.shape(arr))
+print(arr[0])
+# print((len(inputArray.loc[inputArray['Open']]) == longest).all())
+#print(indList[0], len(inputArray.loc['A'].at['Open']))
+# print(inputArray[0:10])
+#print(inputArray.loc['A'].at['Open'])
+gm = GaussianMixture(n_components=3, random_state=0).fit_predict(arr)
